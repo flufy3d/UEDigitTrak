4f44e9119891e0375954f13049da5b3f89d23832
flufy3d <flufy3d@gmail.com>
2016-12-06 16:46:47 +0800

    fix build error

--- Source/UEDigitTrak/Private/UEDigitTrakPrivatePCH.h
+++ Source/UEDigitTrak/Private/UEDigitTrakPrivatePCH.h
@@ -1,6 +1,11 @@
 // Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 #pragma once
 
+#include "Core.h"
+#include "CoreUObject.h"
+#include "Engine.h"
+#include "Engine/Console.h"
+
 #include "IUEDigitTrak.h"
 
 // You should place include statements to your module's private header files here.  You only need to
